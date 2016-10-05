70bdc678be827b7a0c7cecce561927f830f0cb39
rubal501 <rubal820@gmail.com>
2016-10-05 18:52:10 -0500

    fase 1

--- intento.py
+++ intento.py
@@ -1,10 +1,4 @@
 import random
 import time
-i = int(random.uniform(0, 100))
-print "pienso en un numero, adivina cual es: "
-time.sleep(3)
-a = raw_input("intenta :")
-if a == i:
-    print "adivinaste humano"
-elif a != i:
-    print "intenta de nuevo humano"
+print "i am going to think in a random number."
+o = int(raw_input("which range (10,100,1000) should i use?"))
