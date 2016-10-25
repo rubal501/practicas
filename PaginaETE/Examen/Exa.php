204f6142754b7d8871d22cd598b3cf7f1605505a
rubal501 <rubal820@gmail.com>
2016-10-25 16:25:09 -0500

    Nuevo php

---
+++ PaginaETE/Examen/Exa.php
@@ -0,0 +1,7 @@
+
+
+Su nombre es: <?php echo htmlspecialchars($_POST['nom']); ?> .<br>
+Su numero de cuenta: <?php echo $_POST['num'];  ?>.<br>
+Su grupo: <?php echo $_POST['grupo'];  ?>.<br>
+<?php echo "Sus respuestas fueron capturadas"; ?><br>
+<?php echo "Que la fuerza te acompane"; ?>
