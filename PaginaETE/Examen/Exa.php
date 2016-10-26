ab144c35662b08e0962261ff0b47abe88547cec7
rubal501 <rubal820@gmail.com>
2016-10-25 21:49:56 -0500

    Nueva branch

--- PaginaETE/Examen/Exa.php
+++ PaginaETE/Examen/Exa.php
@@ -1,7 +1,8 @@
 
 
-Su nombre es: <?php echo htmlspecialchars($_POST['nom']); ?> .<br>
+<!--/*Su nombre es: <?php echo htmlspecialchars($_POST['nom']); ?> .<br>
 Su numero de cuenta: <?php echo $_POST['num'];  ?>.<br>
 Su grupo: <?php echo $_POST['grupo'];  ?>.<br>
 <?php echo "Sus respuestas fueron capturadas"; ?><br>
-<?php echo "Que la fuerza te acompane"; ?>
+<?php echo "Que la fuerza te acompane"; ?>*/-->
+//estas lineas se suprimen de momento para que no anden jodiendo
