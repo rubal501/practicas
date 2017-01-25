1329b09b98143370be0b6e43f3f0b9e7cd16502b
rubal501 <your_email@example.com>
2017-01-24 20:49:10 -0600

    commit sin sentido

--- fadz.c
+++ fadz.c
@@ -1,36 +1,32 @@
+int ledRojo = 8;int ledVerde = 9; ledAmarillo = 10;
+int mssg = 0; //variable para guardar el mensaje
 
-int cont = 0;
-int kont = 255;
-int e = 51;
-//cont se refiere al contador del led 10
-//kont es el contador de led 11
-
-void setup() {
-
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  //define el modo de los leds
+ 
+void setup()
+{
+   pinMode(ledRojo, OUTPUT);
+   pinMode(ledAmarillo, OUTPUT);
+   pinMode(ledVerde, OUTPUT); //establecemos 13 como salida
+   Serial.begin(9600); //iniciando Serial
 }
-
-
-void loop() {
-
-    analogWrite(10,cont);
-    
-    analogWrite(11,kont);
-    delay(1000);
  
-    cont = cont +e;
-    kont = kont -e;
-
-    //cuando los contadores lleguen al final del ciclo esto hace que se reinicie
-    //creo que podria hacerse en un while
-    if(cont==255||cont==0){
-      e = -1*e;
-      /*
-    wdasdfasd   dasdasddas  asddasd*/
-    }
+void loop()
+{
+   if (Serial.available() > 0)
+   {
+      mssg = Serial.read(); //leemos el serial
  
-
-
+      if(mssg == 'r')
+      {
+         digitalWrite(ledRojo, HIGH); //si entra una 'e' encendemos
+      }
+      else if(mssg == 'a')
+      {
+         digitalWrite(ledAmarillo, HIGH); //si entra una 'a' apagamos
+      }
+      else if (mssg = 'v')
+      {
+      	digitalWrite(ledVerde, HIGH);
+      }
+   }
 }
