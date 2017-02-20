e7da97c50773822cfdc8fa5e8d346d0afa6ed0b0
rubal501 <your_email@example.com>
2017-02-19 22:00:03 -0600

    Me voy a dormir

--- CarroBluethoth/CarroBluethoth.ino
+++ CarroBluethoth/CarroBluethoth.ino
@@ -1,9 +1,14 @@
+#include <SoftwareSerial.h>
 #define motor1 3
 #define motor2 6
 #define M1dir1 4//motor1 con direccion 1
-#define M1dir2 5
-#define M2dir1 7
-#define M2dir2 8
+#define M1dir2 5//motor1 con direccion 2 
+#define M2dir1 7//motor2 con direccion 1
+#define M2dir2 8//motor2 con direccion 2
+/*ojo la direccion 1 es hacia adelante, mientras que la
+direccion dos va hacia atras*/
+SoftwareSerial BT(9,10);
+char dato;
 void setup() {
   // put your setup code here, to run once:
   pinMode(motor1, OUTPUT);
@@ -12,25 +17,45 @@ void setup() {
   pinMode(M1dir2, OUTPUT);
   pinMode(M2dir1, OUTPUT);
   pinMode(M2dir2, OUTPUT);
-
+  //se inicializa cada uno de los puertos
+  BT.begin(9600);
 }
 void MoverHaciaAdelante(void){
   digitalWrite(motor1, LOW);
   digitalWrite(motor2, LOW);
+  //delay(100);
   digitalWrite(M1dir2, LOW);
   digitalWrite(M1dir1, HIGH);
   digitalWrite(M2dir2, LOW);
   digitalWrite(M2dir1, HIGH);
-  analogWrite(motor1, 200);
-  analogWrite(motor2, 200);
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, HIGH);
   delay(10000);
-
+}
+void MoverHaciaAtras(void){
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  //delay(100);
+  digitalWrite(M1dir2, HIGH);
+  digitalWrite(M1dir1, LOW);
+  digitalWrite(M2dir2, HIGH);
+  digitalWrite(M2dir1, LOW);
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, HIGH);
+  delay(10000);
 
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  MoverHaciaAdelante();
+  if(BT.available() > 0)  //revisa si se recibe datos
+   {
+      dato = BT.read();
+      if(dato == '1')
+         MoverHaciaAdelante();
+      else if(dato == '0')
+         MoverHaciaAtras();    
+   }
 
 }
+//NOTA AL CALCE: puede que sea mas eficaz si uso un switch
