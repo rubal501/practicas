370586bc2c3554f7a5661e3426a989332c9d6ccb
rubal501 <rubal820@gmail.com>
2017-02-26 19:10:06 -0600

    cosas bonitas

--- Astronomia/Brazo mecanico/sketch_jan25a/sketch_jan25a.ino
+++ Astronomia/Brazo mecanico/sketch_jan25a/sketch_jan25a.ino
@@ -1,32 +1,32 @@
 int angulo1, angulo2, diferencia, B, A;
 int pin1 = 6;
 int pin2 = 5;
-int pinEntrada = 0;
+int pinEntrada = A0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pinEntrada, INPUT);
+  pinMode(A1, INPUT);
   Serial.begin(9600);  
-map(angulo1,0,1024,0,100);
+  //map(angulo1,0,1024,0,100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   angulo1 = analogRead(pinEntrada);
   //falta una manera para que interactue con el usuario
-  angulo2 = Serial.read();
+  angulo2 = analogRead(A1);
   diferencia = angulo2 - angulo1;
   if (diferencia > 0)
    {
-     B = 0;
-    A = diferencia;
+   
+    analogWrite(pin2, 0);
     analogWrite(pin1, diferencia);
    }
    if (diferencia < 0)
     {
-      A = 0;
-      B = -1* diferencia;
+      analogWrite(pin1, 0);
       analogWrite(pin2, diferencia);
     }
     if (diferencia == 0)
