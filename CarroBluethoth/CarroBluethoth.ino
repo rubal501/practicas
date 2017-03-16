04921319f352d68ddc5361e2f1d908f4b215c80e
rubal501 <rubal820@gmail.com>
2017-03-16 16:10:48 -0600

    mejoras

--- CarroBluethoth/CarroBluethoth.ino
+++ CarroBluethoth/CarroBluethoth.ino
@@ -6,11 +6,11 @@
 #define M2dir1 7//motor2 con direccion 1
 #define M2dir2 8//motor2 con direccion 2
 /*ojo la direccion 1 es hacia adelante, mientras que la
-direccion dos va hacia atras*/
-SoftwareSerial BT(9,10);
+  direccion dos va hacia atras*/
+SoftwareSerial BT(9, 10);//el puto rx y tx va al revez pelotudo
 char dato;
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once:++++
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
   pinMode(M1dir1, OUTPUT);
@@ -19,23 +19,20 @@ void setup() {
   pinMode(M2dir2, OUTPUT);
   //se inicializa cada uno de los puertos
   BT.begin(9600);
+  Serial.begin(9600);
 }
-void MoverHaciaAdelante(void){
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor2, LOW);
-  //delay(100);
+void MoverHaciaAdelante(void) {
   digitalWrite(M1dir2, LOW);
   digitalWrite(M1dir1, HIGH);
   digitalWrite(M2dir2, LOW);
   digitalWrite(M2dir1, HIGH);
-  digitalWrite(motor1, HIGH);
-  digitalWrite(motor2, HIGH);
+  delay(1000);
 }
-void Frenar (void){
+void Frenar (void) {
   digitalWrite(motor1, LOW);
   digitalWrite(motor2, LOW);
 }
-void MoverHaciaAtras(void){
+void MoverHaciaAtras(void) {
   digitalWrite(motor1, LOW);
   digitalWrite(motor2, LOW);
   //delay(100);
@@ -45,23 +42,62 @@ void MoverHaciaAtras(void){
   digitalWrite(M2dir1, LOW);
   digitalWrite(motor1, HIGH);
   digitalWrite(motor2, HIGH);
+  delay(1000);
+
+}
+void MoverHaciaIzquierda(void) {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  //delay(100);
+  digitalWrite(M1dir2, HIGH);
+  digitalWrite(M1dir1, LOW);
+  digitalWrite(M2dir2, HIGH);
+  digitalWrite(M2dir1, HIGH);
+  analogWrite(motor1, 150);
+  digitalWrite(motor2, HIGH);
+  delay(1000);
+
+}
+void MoverHaciaDerecha(void) {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  //delay(100);
+  digitalWrite(M1dir2, HIGH);
+  digitalWrite(M1dir1, LOW);
+  digitalWrite(M2dir2, HIGH);
+  digitalWrite(M2dir1, HIGH);
+  analogWrite(motor2, 150);
+  digitalWrite(motor1, HIGH);
+  delay(1000);
 
 }
 
 
 void loop() {
-  if(BT.available() > 0)  //revisa si se recibe datos
-   {
-      switch(dato){
-        case 'a':
-          MoverHaciaAdelante();
-        case 'b':
-          MoverHaciaAtras();
-        case 'f':
-          Frenar();
-        default:
-          Frenar();
-      }
-   }
+  if (BT.available() > 0) //revisa si se recibe datos
+  {
+    dato = BT.read();
+    Serial.println(dato);
+    if (dato == 'a') {
+      digitalWrite(motor1, HIGH);
+      digitalWrite(motor2, HIGH);
+      MoverHaciaAdelante();
+    }
+    if (dato == 'r') {
+      MoverHaciaDerecha();
+    }
+    if (dato == 'l') {
+      MoverHaciaIzquierda();
+    }
+    if (dato == 'b') {
+      digitalWrite(motor1, HIGH);
+      digitalWrite(motor2, HIGH);
+      MoverHaciaAtras();
+    }
+    if (dato == 'f') {
+      Frenar();
+    }
+
+  }
 
 }
